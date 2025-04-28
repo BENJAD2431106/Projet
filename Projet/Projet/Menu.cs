071181632ea66d5ad2fa22e2b7e33eeac0efd558
Oleksandr Bilynets <2431570@etu.cegepjonquiere.ca>
2025-04-28 08:54:30 -0400

    Partie 1

---
+++ Projet/Projet/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    public partial class Menu
+    {
+        List<Plat> plats = new List<Plat>();
+
+        public Menu(){ }
+
+        public void AjouterPlat(Plat plat)
+        {
+            plats.Add(plat);
+        }
+
+        public void SupprimerPlat(Plat plat)
+        {
+            plats.Remove(plat);
+        }
+        
+        public override string ToString()
+        {
+            string info = "";
+            info += "Menu J'A Resto\n";
+            foreach (var plat in plats)
+            {
+                info += plat + "\n";
+            }
+            return info;
+        }
+    }
+}
